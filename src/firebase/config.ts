9a7dfb61453356b0feb84cbe432b9e4db700d90e
shivam kaushik <shivamkaushik1771@gmail.com>
2025-10-29 15:48:14 +0530

    Replace Firebase config with placeholder values
    Updated Firebase configuration to use placeholder values. Use Your To Use Project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,11 +5,11 @@
 // This configuration is used for the Firebase SDKs on the client-side.
 // It is public and does not contain any sensitive information.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: use your,
+  authDomain: use your,
+  projectId: use your,
+  storageBucket: use your,
+  messagingSenderId: use your,
+  appId: use your,
+  measurementId: use your,
 };
